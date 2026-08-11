dc75e71d50eb088c7f73fe8faf04eda77b4cdf5a
Dev Niranjan <devniranjan0111@gmail.com>
2026-08-11 19:52:31 +0530

    added vercel.json and improved verify.jsx

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -3,12 +3,12 @@ import { ShopContext } from "../context/ShopContext.jsx";
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import axios from "axios";
-import {toast} from "react-toastify"
+import { toast } from "react-toastify";
 
 const Verify = () => {
     const { navigate, token, setCartItems, backendUrl } =
         useContext(ShopContext);
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const success = searchParams.get("success");
     const orderId = searchParams.get("orderId");
     const verifyPayment = async () => {
@@ -34,7 +34,11 @@ const Verify = () => {
     useEffect(() => {
         verifyPayment();
     }, [token]);
-    return <div>Verify</div>;
+    return (
+        <div className="min-h-[60vh] flex items-center justify-center">
+            <p className="text-gray-600">Processing payment...</p>
+        </div>
+    );
 };
 
 export default Verify;
