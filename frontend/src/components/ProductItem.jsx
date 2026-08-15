96fdc16a01943302d16ac62d6111dea1a9b48802
Dev Niranjan <devniranjan0111@gmail.com>
2026-08-15 16:29:34 +0530

    add scroll to top on product click

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -5,7 +5,11 @@ import { Link } from "react-router-dom";
 const ProductItem = ({ id, image, name, price }) => {
     const { currency } = useContext(ShopContext);
     return (
-        <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
+        <Link
+            onClick={() => scrollTo(0, 0)}
+            to={`/product/${id}`}
+            className="text-gray-700 cursor-pointer"
+        >
             <div className="overflow-hidden">
                 <img
                     src={image[0]}
@@ -14,7 +18,10 @@ const ProductItem = ({ id, image, name, price }) => {
                 />
             </div>
             <p className="pt-3 pb-1 text-sm">{name}</p>
-            <p className="text-sm font-medium">{currency}{price}</p>
+            <p className="text-sm font-medium">
+                {currency}
+                {price}
+            </p>
         </Link>
     );
 };
